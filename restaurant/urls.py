e4df5426c197592eb43c842b6fa432d78b356b7e
wizwiz <abezzi@outlook.cl>
2025-04-20 18:10:50 -0400

    feat: booking crud

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import SingleMenuItemView, MenuItemsView
 
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from django.contrib.auth.models import User
+
+from .models import Booking, MenuItem
+from .serializers import BookingSerializer, MenuItemSerializer, UserSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 def index(request):
     return render(request, "index.html", {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class UserViewSet(ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [IsAuthenticated] 
